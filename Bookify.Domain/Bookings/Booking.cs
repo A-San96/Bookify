cd41d446d7cddf55c373ee26da5b5a516744d4e0
A-San96 <assanediatta96@gmail.com>
2023-10-20 12:44:07 +0000

    refactor: use pricing service instead of pricing details on booking factory method

--- Bookify.Domain/Bookings/Booking.cs
+++ Bookify.Domain/Bookings/Booking.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings.Events;
 using Bookify.Domain.Shared;
 using System;
@@ -50,15 +51,18 @@ public sealed class Booking : Entity
     public DateTime? CancelledOnUtc { get; private set; }
 
     public static Booking Reserve(
-        Guid apartmentId,
+        Apartment apartment,
         Guid userId,
         DateRange duration, 
         DateTime utcNow,
-        PricingDetails pricingDetails)
+        PricingService pricingService)
     {
+
+        var pricingDetails = pricingService.CalculatePrice(apartment, duration);
+
         var booking = new Booking(
             Guid.NewGuid(), 
-            apartmentId, 
+            apartment.Id, 
             userId, 
             duration,
             pricingDetails.PriceForPeriod,
@@ -69,6 +73,8 @@ public sealed class Booking : Entity
             utcNow);
 
         booking.RaiseDomainEvent(new BookingReservedDomainEvent(booking.Id));
+
+        // apartment.LastBookedOnUtc = utcNow;
 
         return booking;
     }
