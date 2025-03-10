16057e9743444dc9ecce11383f057123b0d4dd54
A-San96 <assanediatta96@gmail.com>
2025-03-10 21:37:37 +0400

    feat(domain): Add apartments entity in domain layer

---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,20 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    public static Currency Usd { get; } = new("USD");
+    public static Currency Eur { get; } = new("EUR");
+    public static Currency None { get; } = new("");
+
+    private Currency(string code) => Code = code;
+
+    public string Code { get; init; }
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code)
+            ?? throw new ApplicationException($"The Currency with code {code} is invalid");
+    }
+
+    public static readonly IReadOnlyCollection<Currency> All = [Usd, Eur];
+}
