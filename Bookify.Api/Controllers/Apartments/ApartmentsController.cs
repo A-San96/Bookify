62f9473913048c046b4c69e3341f738a5f1e7d70
A-San96 <assanediatta96@gmail.com>
2025-03-26 15:17:15 +0400

    feat(Api): Add api endpoints

---
+++ Bookify.Api/Controllers/Apartments/ApartmentsController.cs
@@ -0,0 +1,30 @@
+﻿using Bookify.Application.Apartments.SearchApartments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers.Apartments;
+
+[ApiController]
+[Route("api/apartments")]
+public class ApartmentsController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ApartmentsController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SearchApartments(
+        DateOnly startDate,
+        DateOnly endDate,
+        CancellationToken cancellation
+    )
+    {
+        var query = new SearchApartmentsQuery(startDate, endDate);
+        var result = await _sender.Send(query, cancellation);
+
+        return Ok(result.Value);
+    }
+}
