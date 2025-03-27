4d30a4cb1189ebe08621d09dc6d592b01377a8ef
A-San96 <assanediatta96@gmail.com>
2025-03-27 16:15:30 +0400

    feat: configure auth middleware and services

---
+++ Bookify.Infrastructure/Authentication/AuthenticationOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookify.Infrastructure.Authentication;
+
+public sealed class AuthenticationOptions
+{
+    public string Audience { get; init; } = String.Empty;
+    public string MetadataUrl { get; init; } = String.Empty;
+    public bool RequireHttpsMetadata { get; init; }
+    public string ValidIssuer { get; set; } = String.Empty;
+}
